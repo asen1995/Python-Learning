dc0b9c1b77bd2595ffdc6b80a03298ff788369ae
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-09-01 16:28:18 +0300

    experiencing with function and seperate files

---
+++ DataPrinter.py
@@ -0,0 +1,14 @@
+
+def printme( str ):
+   "This prints a passed string into this function"
+   print (str)
+   return;
+
+
+def printFullList(thelist):
+    for element in thelist:
+        print(element)
+
+def printListFromTo(thelist,fromIndex,toIndex):
+    print(fromIndex,toIndex)
+    print(list[fromIndex:toIndex])
