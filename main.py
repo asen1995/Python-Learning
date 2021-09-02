9ad6014ec6a44110216bbc9064aa5afcc02a4989
Asen Nikolaev <asen.nikolaev95@abv.bg>
2021-09-02 15:03:45 +0300

    orginize python scripts in seperate packages/folders

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import DataPrinter as dp
-import exceptionExample as exceptionExample
-import FileManager as fileManager
-from Employee import Employee
+import filemanagement.DataPrinter as dp
+import exception.exceptionExample as exceptionExample
+import filemanagement.FileManager as fileManager
+from model.Employee import Employee
 
 def main():
 
     # dp.printme("str")
     #
-    # # PRINT LIST NUMBERS
+    # PRINT LIST NUMBERS
     # listOfNumbers = ['2', '5', '7', '37', '67', '46', '745', '732', '734', '756']
     # dp.printFullList(listOfNumbers)
     # dp.printListFromTo(listOfNumbers, 3, 8)
-
+    #
     # fileManager.writeIntoFile("asen.txt","content to write and read after that")
     # #fileManager.deleteFile("asen.txt")
     # fileManager.readFile("asen.txt")
