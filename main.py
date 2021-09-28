600a30b9a3d77cdc5e1bfa8a017ee478d8c4a8d6
Asen Nikolaev <asen.nikolaev95@abv.bg>
2021-09-28 16:28:24 +0300

    list methods and example

--- main.py
+++ main.py
@@ -55,8 +55,18 @@ def main():
     #     print('The count is:', count)
     #     count = count + 1
 
+    #     datastructures
 
-
+    list = ["apple", "banana", "cherry"]
+    print(list)
+    print("len is ", len(list))
+    print(type(list))
+    list.append("orange")
+    print(list)
+    list.insert(0,"Asen")
+    print(list)
+    list.remove("Asen")
+    print(list)
 
 main()
 
